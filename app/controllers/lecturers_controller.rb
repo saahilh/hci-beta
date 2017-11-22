14d2e66b09de083f2005baee6460595dfe2f2dc0
Saahil Hamayun <saahilhamayun@hotmail.com>
2017-11-22 15:52:58 -0500

    fixed status messages & polling

--- app/controllers/lecturers_controller.rb
+++ app/controllers/lecturers_controller.rb
@@ -2,14 +2,14 @@ class LecturersController < ActionController::Base
   def login
     lecturer = Lecturer.where(email: params[:email])
     if(lecturer.count != 0 && lecturer.first.password==params[:pw])
-      redirect_to "/lecturers/#{lecturer.first.id}"
+      render json: { data: { redirect: "/lecturers/#{lecturer.first.id}" } }
     else
-      render '/partials/message', locals:{ msg: "Invalid credentials entered", href: "/index.html" }
+      render json: { data: { msg: "Invalid credentials entered" } }
     end
   end
 
   def show
-  	render 'prof-ile', locals:{lecturer: Lecturer.find(params[:id])}
+  	render 'prof-ile', locals:{ lecturer: Lecturer.find(params[:id]) }
   end
 
   def create
