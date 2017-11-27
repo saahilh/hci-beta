1c37b98a687c8f0ef05fd7c871a092c47df701c1
Saahil Hamayun <saahilhamayun@hotmail.com>
2017-11-26 19:47:20 -0500

    various and many bug fixes

--- app/channels/course_channel.rb
+++ app/channels/course_channel.rb
@@ -1,5 +1,7 @@
 class CourseChannel < ApplicationCable::Channel
 	def subscribed
-		stream_for Course.where(name: params[:room]).first
+		course = Course.where(name: params[:room]).first
+		stream_for course
+		CourseChannel.broadcast_to(course, connected: true)
 	end
 end
