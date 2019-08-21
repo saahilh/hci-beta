05dc7b06ddae07cb1d682745f555c904b79cbcae
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-08-21 14:04:06 -0400

    Changed student flagged default to array because it was just a list of questions flagged

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190815205207) do
+ActiveRecord::Schema.define(version: 20190821180207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20190815205207) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "question_data", default: "{}"
-    t.text "flagged", default: "{}"
+    t.text "flagged", default: "[]"
     t.text "poll_data", default: "{}"
   end
 
